7cbb2ec7d25bf4cb061efcab6a6f713c29e1808a
EvaXausa <xausa.eva@gmail.com>
2022-12-12 22:41:21 +0100

    js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
 per i multipli di 5 stampi “Buzz” al posto del numero.
  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
  */
+
+const list = document.getElementById("numbers-list");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}

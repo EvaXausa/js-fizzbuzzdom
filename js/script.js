7253b959384701b16cb94ed460bf869cf1c91d2e
EvaXausa <xausa.eva@gmail.com>
2022-12-29 14:21:56 +0100

    correction

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 console.log("JS OK");
 /*
-Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
- per i multipli di 3 stampi “Fizz” al posto del numero
-per i multipli di 5 stampi “Buzz” al posto del numero.
- Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+Scrivi un programma che stampi in console i numeri da 1 a 100, 
+- per i multipli di 3 stampa “Fizz”
+- per i multipli di 5 stampa “Buzz”
+ Per i numeri che sono sia multipli di 3 che di 5 stampa “FizzBuzz”.
  */
 
 const list = document.getElementById("numbers-list");
+let items = "";
 
 for (let i = 1; i <= 100; i++) {
+  let content = i;
+
   if (i % 15 === 0) {
-    console.log("FizzBuzz");
+    content = `FizzBuzz`;
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    content = `Buzz`;
   } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
+    content = `Fizz`;
   }
+
+  items += `<li>${content}</li>`;
 }
+
+list.innerHTML = items;
